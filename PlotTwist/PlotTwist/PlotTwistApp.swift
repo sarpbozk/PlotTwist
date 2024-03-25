d8312cad735c5b185fe9f297ed6c4db088e3a40c
Sarp Bozkurt <sarpbozk@gmail.com>
2024-03-25 17:18:02 +0300

    Initial Commit

---
+++ PlotTwist/PlotTwist/PlotTwistApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotTwistApp.swift
+//  PlotTwist
+//
+//  Created by Sarp Bozkurt on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlotTwistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
